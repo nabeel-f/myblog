ce0230e3e5ada280d7669607359ea68afba671c4
Nabeel <nabeel_f@hotmail.com>
2020-05-21 22:22:55 -0600

    First Commit

---
+++ src/build/precache-manifest.d7df2785852383679b9795bdd8e64853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea89c11e6e383fa4dfc7a806913d1312",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa544da713672801d569",
+    "url": "/static/css/main.b2c081c9.chunk.css"
+  },
+  {
+    "revision": "363b3a2220db887d0220",
+    "url": "/static/js/2.78bea10c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.78bea10c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa544da713672801d569",
+    "url": "/static/js/main.069fcae3.chunk.js"
+  },
+  {
+    "revision": "2c5dc751d0cfe89d3f01",
+    "url": "/static/js/runtime-main.5dc02bb2.js"
+  }
+]);
